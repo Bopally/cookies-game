ef3ea416c01d2cc62ff6b2d0b1c0202fef3d586a
Bopally <estelleb0707@gmail.com>
2024-10-14 12:49:09 +0200

    Add player functionalities

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ class Player {
     this.domElement = null;
 
     this.createDomElement();
+    this.addKeyboardControls();
   }
   createDomElement() {
     this.domElement = document.createElement("div");
@@ -20,9 +21,33 @@ class Player {
     const board = document.getElementById("board");
     board.appendChild(this.domElement);
   }
+
+  // Movments of Player
+  moveRight() {
+    if (this.positionX < 100 - this.width) {
+      this.positionX++;
+      this.domElement.style.left = this.positionX + "vw";
+    }
+  }
+  moveLeft() {
+    if (this.positionX > 0) {
+      this.positionX--;
+      this.domElement.style.left = this.positionX + "vw";
+    }
+  }
+
+  addKeyboardControls() {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        this.moveRight();
+      } else if (e.key === "ArrowLeft") {
+        this.moveLeft();
+      }
+    });
+  }
 }
 
-//Event Listener
+//Event Listener for our Dom Content
 document.addEventListener("DOMContentLoaded", () => {
   const player = new Player();
 });
